69145845e0d09a05b73a8b7e275aedbd6b22266f
dhyeypatel112020 <87629011+dhyeypatel112020@users.noreply.github.com>
2022-11-23 08:37:48 +0530

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,9 @@
+nodemailer=this is the library which is used to send mail in your application using SMTP(Simple Mail Tranfer Protocol)
+
+Step1 - we will install nodemailer in package.json
+
+Step2 - create transporter
+
+Step3 - app password
+
+Step4 - create api
---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+const { v4: uuidv4 } = require("uuid");
+
+const io = require("socket.io")(server, {
+    cors: {
+        origin: '*'
+    }
+});
+
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+    debug: true,
+});
+
+app.use("/peerjs", peerServer);
+
+
+var nodemailer=require("nodemailer");
+const { errorMonitor } = require("events");
+
+var tranporter=nodemailer.createTransport({
+    port:465,
+    host:"smtp.gmail.com",
+    auth:{
+        user:"dhyeypatel112020@gmail.com",
+        pass:"gsiccbicylsbqiov"
+    },
+    secure:true
+})
+
+
+app.get("/", (req, res) => {
+    res.redirect(`/${uuidv4()}`);
+});
+
+app.get("/:room", (req, res) => {
+    res.render("index", { roomId: req.params.room });
+});
+
+app.post("/send-mail", (req, res) => {
+    const to=req.body.to
+    const url=req.body.url
+    const subject=req.body.subject
+    const maildata={
+        from:"dhyeypatel112020@gmail.com",
+        to:to,
+        subject:"Please Join the Video shat room    ",
+        html:`<i>please join: ${url}</i>`
+    }
+    tranporter.sendMail(maildata,(error,info)=>{
+        if(error){
+            return console.log(error)
+        }
+        res.status(200).send({mesage:"mail Send",message_id:info.messageId})
+    })
+});
+
+io.on("connection", (socket) => {
+    socket.on("join-room", (roomId, userId, userName) => {
+        socket.join(roomId);
+        io.to(roomId).emit("user-connected", userId);
+        socket.on("message", (message) => {
+            io.to(roomId).emit("createMessage", message, userName);
+        });
+    });
+});
+
+server.listen(process.env.PORT || 3030);
